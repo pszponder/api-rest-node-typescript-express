84aaf7f28f4f2398863ccdc494a34ee079c8a552
Piotr Szponder <14116413+pszponder@users.noreply.github.com>
2023-03-05 22:19:24 -0700

    feat: remove unused middleware

--- src/api/middleware/_middlewares.ts
+++ src/api/middleware/_middlewares.ts
@@ -1,5 +1,4 @@
-import { wrapAsyncRequestHandler } from "./asyncRequestHandlerExpress.middleware.js";
 import { errorHandler } from "./errorHandler.middleware.js";
 import { notFound } from "./notFound.middleware.js";
 
-export { errorHandler, notFound, wrapAsyncRequestHandler };
+export { errorHandler, notFound };
